ebb239a7637b972c667cac5e626fa9264a3f473b
angcyo <angcyo@126.com>
2016-11-13 11:54:41 +0800

    add view pager demo

--- demo/src/main/java/com/angcyo/demo/uiview/ViewPagerView.java
+++ demo/src/main/java/com/angcyo/demo/uiview/ViewPagerView.java
@@ -1,8 +1,13 @@
 package com.angcyo.demo.uiview;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.os.SystemClock;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.CompoundButton;
 import android.widget.FrameLayout;
 import android.widget.RadioButton;
@@ -13,6 +18,9 @@ import com.angcyo.demo.R;
 import com.angcyo.library.utils.L;
 import com.angcyo.uiview.base.UIBaseView;
 import com.angcyo.uiview.container.ILayout;
+import com.angcyo.uiview.container.UIContainer_back;
+
+import java.util.Random;
 
 import butterknife.BindView;
 import butterknife.OnCheckedChanged;
@@ -22,6 +30,9 @@ import butterknife.OnCheckedChanged;
  */
 
 public class ViewPagerView extends UIBaseView {
+
+    @BindView(R.id.view_pager)
+    ViewPager mViewPager;
 
     @BindView(R.id.radio_group)
     RadioGroup mRadioGroup;
@@ -44,15 +55,77 @@ public class ViewPagerView extends UIBaseView {
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 L.w("onCheckedChanged:" + checkedId + " " + ((TextView) group.findViewById(checkedId)).getText()
                         + " " + ((RadioButton) group.findViewById(checkedId)).isChecked());
+
             }
         });
 //        mRadioGroup.check(R.id.home_radio);
 
         mHomeRadio.setChecked(true);
+
+        mViewPager.setOffscreenPageLimit(1);
+        mViewPager.setAdapter(new ViewPagerAdapter());
+        mViewPager.addOnPageChangeListener(new android.support.v4.view.ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                L.w("PagerAdapter", "onPageSelected: " + position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        mViewPager.setCurrentItem(2);
+
+        mHomeRadio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewPager.setCurrentItem(3);
+            }
+        });
     }
 
     @OnCheckedChanged(R.id.home_radio)
     public void onChecked(CompoundButton view, boolean checked) {
-        L.w("onChecked:" + view.getId() + " " + view.getText() + " " + view.isChecked());
+        L.w("onChecked:" + view.getId() + " " + view.getText() + " " + checked);
+    }
+
+    private class ViewPagerAdapter extends PagerAdapter {
+
+        @Override
+        public int getCount() {
+//            L.w("PagerAdapter", "getCount: ");
+            return 5;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            L.w("PagerAdapter", "instantiateItem: " + position);
+            Random random = new Random(SystemClock.uptimeMillis());
+            View layoutWrapper = new UIContainer_back(mContext);
+//            layoutWrapper.setText("当前页:" + position);
+//            layoutWrapper.setTextColor(Color.WHITE);
+            layoutWrapper.setBackgroundColor(Color.rgb(random.nextInt(255), random.nextInt(255), random.nextInt(255)));
+            container.addView(layoutWrapper, new ViewGroup.LayoutParams(-1, -1));
+            return layoutWrapper;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            L.w("PagerAdapter", "destroyItem: " + position);
+            container.removeView((View) object);
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            //L.w("PagerAdapter", "isViewFromObject: ");
+            return view == object;
+        }
     }
 }
+
