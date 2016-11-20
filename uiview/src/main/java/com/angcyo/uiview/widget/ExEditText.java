46d1a781efe8196c3f29c8ea7ce20fe56114707f
angcyo <angcyo@126.com>
2016-11-21 00:25:16 +0800

    add edit delete state

--- uiview/src/main/java/com/angcyo/uiview/widget/ExEditText.java
+++ uiview/src/main/java/com/angcyo/uiview/widget/ExEditText.java
@@ -2,6 +2,8 @@ package com.angcyo.uiview.widget;
 
 import android.content.Context;
 import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.widget.AppCompatEditText;
 import android.text.InputFilter;
 import android.text.TextUtils;
@@ -16,7 +18,9 @@ import com.angcyo.uiview.R;
 
 public class ExEditText extends AppCompatEditText {
 
-    Rect clearRect = new Rect();
+    Rect clearRect = new Rect();//删除按钮区域
+    boolean isDownIn = false;//是否在按钮区域按下
+    Drawable clearDrawable;
 
     public ExEditText(Context context) {
         super(context);
@@ -40,6 +44,10 @@ public class ExEditText extends AppCompatEditText {
             newFilters[filters.length] = new EmojiFilter();
             setFilters(newFilters);
         }
+        clearDrawable = ResourcesCompat.getDrawable(
+                getResources(),
+                R.drawable.base_edit_delete_selector,
+                getContext().getTheme());
     }
 
     @Override
@@ -51,27 +59,51 @@ public class ExEditText extends AppCompatEditText {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        clearRect.set(w - getPaddingRight() - Math.min(w, h), getPaddingTop(), w - getPaddingRight(), Math.min(w, h) - getPaddingBottom());
+        clearRect.set(w - getPaddingRight() - clearDrawable.getIntrinsicWidth(), getPaddingTop(), w - getPaddingRight(), Math.min(w, h) - getPaddingBottom());
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_DOWN && isFocused()) {
-            if (checkClear(event.getX(), event.getY())) {
-                if (!TextUtils.isEmpty(getText())) {
-                    setText("");
-                    return true;
+        if (isFocused()) {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                isDownIn = checkClear(event.getX(), event.getY());
+                updateState(isDownIn);
+            } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+                updateState(checkClear(event.getX(), event.getY()));
+            } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                updateState(false);
+                if (isDownIn && checkClear(event.getX(), event.getY())) {
+                    if (!TextUtils.isEmpty(getText())) {
+                        setText("");
+                        return true;
+                    }
                 }
+                isDownIn = false;
+            } else if (event.getAction() == MotionEvent.ACTION_CANCEL) {
+                updateState(false);
+                isDownIn = false;
             }
         }
         return super.onTouchEvent(event);
+    }
+
+    private void updateState(boolean isDownIn) {
+        final Drawable clearDrawable = getCompoundDrawables()[2];
+        if (clearDrawable == null) {
+            return;
+        }
+        if (isDownIn) {
+            clearDrawable.setState(new int[]{android.R.attr.state_checked});
+        } else {
+            clearDrawable.setState(new int[]{});
+        }
     }
 
     private void checkEdit(boolean focused) {
         if (TextUtils.isEmpty(getText()) || !focused) {
             setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
         } else {
-            setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.base_edit_delete, 0);
+            setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, clearDrawable, null);
         }
     }
 
