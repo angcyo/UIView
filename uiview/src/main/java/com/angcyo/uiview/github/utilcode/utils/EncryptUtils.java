b3532c6583b21e8bfe9842d1e69107c66127abcb
angcyo <angcyo@126.com>
2016-12-06 08:38:00 +0800

    add utils

--- uiview/src/main/java/com/angcyo/uiview/github/utilcode/utils/EncryptUtils.java
+++ uiview/src/main/java/com/angcyo/uiview/github/utilcode/utils/EncryptUtils.java
@@ -13,8 +13,9 @@ import javax.crypto.Cipher;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
-import static com.blankj.utilcode.utils.ConvertUtils.bytes2HexString;
-import static com.blankj.utilcode.utils.ConvertUtils.hexString2Bytes;
+import static com.angcyo.uiview.github.utilcode.utils.ConvertUtils.bytes2HexString;
+import static com.angcyo.uiview.github.utilcode.utils.ConvertUtils.hexString2Bytes;
+
 
 /**
  * <pre>
