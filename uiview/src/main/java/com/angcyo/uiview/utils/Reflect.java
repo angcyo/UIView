d01e243311a7f599f624149dad1200619bc317fe
angcyo <angcyo@126.com>
2016-12-23 22:55:38 +0800

    add utils

--- uiview/src/main/java/com/angcyo/uiview/utils/Reflect.java
+++ uiview/src/main/java/com/angcyo/uiview/utils/Reflect.java
@@ -1,6 +1,9 @@
 package com.angcyo.uiview.utils;
 
+import android.text.TextUtils;
+
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 /**
  * Created by angcyo on 2016-11-26.
@@ -47,5 +50,79 @@ public class Reflect {
     public static String getMethodName() {
         final StackTraceElement[] stackTraceElements = new Exception().getStackTrace();
         return stackTraceElements[1].getMethodName();
+    }
+
+    /**
+     * 通过类对象，运行指定方法
+     *
+     * @param obj        类对象
+     * @param methodName 方法名
+     * @param params     参数值
+     * @return 失败返回null
+     */
+    public static Object invokeMethod(Object obj, String methodName, Object[] params) {
+        if (obj == null || TextUtils.isEmpty(methodName)) {
+            return null;
+        }
+
+        Class<?> clazz = obj.getClass();
+        try {
+            Class<?>[] paramTypes = null;
+            if (params != null) {
+                paramTypes = new Class[params.length];
+                for (int i = 0; i < params.length; ++i) {
+                    paramTypes[i] = params[i].getClass();
+                }
+            }
+            Method method = clazz.getMethod(methodName, paramTypes);
+            method.setAccessible(true);
+            return method.invoke(obj, params);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    /**
+     * 通过反射, 获取obj对象的 指定成员变量的值
+     */
+    public static Object getFieldValue(Object obj, String fieldName) {
+        if (obj == null || TextUtils.isEmpty(fieldName)) {
+            return null;
+        }
+
+        Class<?> clazz = obj.getClass();
+        while (clazz != Object.class) {
+            try {
+                Field field = clazz.getDeclaredField(fieldName);
+                field.setAccessible(true);
+                return field.get(obj);
+            } catch (Exception e) {
+            }
+            clazz = clazz.getSuperclass();
+        }
+        return null;
+    }
+
+    /**
+     * 设置字段的值
+     */
+    public static void setFieldValue(Object obj, String fieldName, Object value) {
+        if (obj == null || TextUtils.isEmpty(fieldName)) {
+            return;
+        }
+
+        Class<?> clazz = obj.getClass();
+        while (clazz != Object.class) {
+            try {
+                Field field = clazz.getDeclaredField(fieldName);
+                field.setAccessible(true);
+                field.set(obj, value);
+                return;
+            } catch (Exception e) {
+            }
+            clazz = clazz.getSuperclass();
+        }
     }
 }
