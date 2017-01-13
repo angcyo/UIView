e047d7d3df7d6327d98b9e7387794322985f2a77
angcyo <angcyo@126.com>
2017-01-13 23:52:03 +0800

    add rx zip demo

--- uidemo/src/main/java/com/angcyo/uidemo/layout/demo/CenterRadioButtonUIView.java
+++ uidemo/src/main/java/com/angcyo/uidemo/layout/demo/CenterRadioButtonUIView.java
@@ -3,12 +3,18 @@ package com.angcyo.uidemo.layout.demo;
 import android.view.LayoutInflater;
 import android.widget.RelativeLayout;
 
+import com.angcyo.library.utils.L;
 import com.angcyo.uidemo.R;
+import com.angcyo.uidemo.T_;
 import com.angcyo.uidemo.layout.demo.view.CenterButton;
 import com.angcyo.uiview.base.UIContentView;
 
 import butterknife.BindView;
 import butterknife.OnClick;
+import rx.Observable;
+import rx.functions.Action1;
+import rx.functions.Func2;
+import rx.schedulers.Schedulers;
 
 /**
  * Copyright (C) 2016,深圳市红鸟网络科技股份有限公司 All rights reserved.
@@ -30,6 +36,35 @@ public class CenterRadioButtonUIView extends UIContentView {
         inflate(R.layout.view_center_button_layout);
     }
 
+    @Override
+    protected void initOnShowContentLayout() {
+        super.initOnShowContentLayout();
+        mCenterButton.setOnCheckedChangeListener(new CenterButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CenterButton buttonView, boolean isChecked) {
+                T_.show(isChecked + "");
+                if (isChecked) {
+                    Observable
+                            .zip(Observable.just("1").subscribeOn(Schedulers.newThread()),
+                                    Observable.just("2").subscribeOn(Schedulers.io()),
+                                    new Func2<Object, Object, Object>() {
+                                        @Override
+                                        public Object call(Object o, Object o2) {
+                                            return null;
+                                        }
+                                    })
+                            .subscribeOn(Schedulers.computation())
+//                            .observeOn(Schedulers.computation())
+                            .subscribe(new Action1<Object>() {
+                                @Override
+                                public void call(Object o) {
+                                    L.e("");
+                                }
+                            });
+                }
+            }
+        });
+    }
 
     @OnClick(R.id.setChecked)
     public void onSetChecked() {
