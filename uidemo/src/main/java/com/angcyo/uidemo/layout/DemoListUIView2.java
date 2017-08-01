2b8a3930feb113b8315b744f2bd9b9293ed7a9bd
angcyo <angcyo@126.com>
2017-08-01 14:35:23 +0800

    add content state demo

--- uidemo/src/main/java/com/angcyo/uidemo/layout/DemoListUIView2.java
+++ uidemo/src/main/java/com/angcyo/uidemo/layout/DemoListUIView2.java
@@ -14,6 +14,7 @@ import com.angcyo.uidemo.layout.demo.AnimatorDemoUIView;
 import com.angcyo.uidemo.layout.demo.BehaviorStickDemoUIView;
 import com.angcyo.uidemo.layout.demo.BehaviorStickDemoUIView2;
 import com.angcyo.uidemo.layout.demo.CenterRadioButtonUIView;
+import com.angcyo.uidemo.layout.demo.ContentStateUIView;
 import com.angcyo.uidemo.layout.demo.CursorLoaderUIView;
 import com.angcyo.uidemo.layout.demo.CustomViewUIView;
 import com.angcyo.uidemo.layout.demo.EmojiUIView;
@@ -618,6 +619,19 @@ public class DemoListUIView2 extends BaseItemUIView {
                                         }
                                     }
                                 });
+                    }
+                });
+            }
+        });
+        items.add(new SingleItem(SingleItem.Type.LINE) {
+
+            @Override
+            public void onBindView(RBaseViewHolder holder, int posInData, Item dataBean) {
+                initItem(holder, posInData + 1 + ".ContentState Demo", new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        startIView(new ContentStateUIView().setEnableClipMode(ClipMode.CLIP_BOTH, v));
                     }
                 });
             }
---
+++ uidemo/src/main/java/com/angcyo/uidemo/layout/demo/ContentStateUIView.kt
@@ -0,0 +1,58 @@
+package com.angcyo.uidemo.layout.demo
+
+import com.angcyo.uidemo.R
+import com.angcyo.uidemo.layout.base.BaseItemUIView
+import com.angcyo.uiview.base.Item
+import com.angcyo.uiview.base.SingleItem
+import com.angcyo.uiview.recycler.RBaseViewHolder
+
+/**
+ * Copyright (C) 2016,深圳市红鸟网络科技股份有限公司 All rights reserved.
+ * 项目名称：
+ * 类的描述：
+ * 创建人员：Robi
+ * 创建时间：2017/08/01 14:03
+ * 修改人员：Robi
+ * 修改时间：2017/08/01 14:03
+ * 修改备注：
+ * Version: 1.0.0
+ */
+class ContentStateUIView : BaseItemUIView() {
+    override fun createItems(items: MutableList<SingleItem>?) {
+        items?.add(object : SingleItem() {
+            override fun onBindView(holder: RBaseViewHolder, posInData: Int, dataBean: Item?) {
+                val itemInfo = holder.item(R.id.base_item_info_layout)
+                itemInfo.setItemText("显示加载状态")
+                itemInfo.setOnClickListener { showLoadLayout() }
+            }
+        })
+        items?.add(object : SingleItem() {
+            override fun onBindView(holder: RBaseViewHolder, posInData: Int, dataBean: Item?) {
+                val itemInfo = holder.item(R.id.base_item_info_layout)
+                itemInfo.setItemText("显示空数据状态")
+                itemInfo.setOnClickListener { showEmptyLayout() }
+            }
+        })
+        items?.add(object : SingleItem() {
+            override fun onBindView(holder: RBaseViewHolder, posInData: Int, dataBean: Item?) {
+                val itemInfo = holder.item(R.id.base_item_info_layout)
+                itemInfo.setItemText("显示无网络状态")
+                itemInfo.setOnClickListener { showNonetLayout { showContentLayout() } }
+            }
+        })
+        items?.add(object : SingleItem() {
+            override fun onBindView(holder: RBaseViewHolder, posInData: Int, dataBean: Item?) {
+                val itemInfo = holder.item(R.id.base_item_info_layout)
+                itemInfo.setItemText("显示错误状态")
+                itemInfo.setOnClickListener { showErrorLayout() }
+            }
+        })
+        items?.add(object : SingleItem() {
+            override fun onBindView(holder: RBaseViewHolder, posInData: Int, dataBean: Item?) {
+                val itemInfo = holder.item(R.id.base_item_info_layout)
+                itemInfo.setItemText("显示内容状态")
+                itemInfo.setOnClickListener { showContentLayout() }
+            }
+        })
+    }
+}
