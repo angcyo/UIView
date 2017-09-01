63dea6ed98f5b8abafb44da4086bc96e44ce00e0
angcyo <angcyo@126.com>
2017-09-01 19:19:44 +0800

    fix demo

--- uidemo/src/main/java/com/angcyo/uidemo/layout/demo/PasswordInputUIView.kt
+++ uidemo/src/main/java/com/angcyo/uidemo/layout/demo/PasswordInputUIView.kt
@@ -1,10 +1,12 @@
 package com.angcyo.uidemo.layout.demo
 
+import android.text.method.KeyListener
 import com.angcyo.uidemo.R
 import com.angcyo.uidemo.layout.base.BaseItemUIView
 import com.angcyo.uiview.base.Item
 import com.angcyo.uiview.base.SingleItem
 import com.angcyo.uiview.recycler.RBaseViewHolder
+import com.angcyo.uiview.widget.PasswordInputEditText
 
 /**
  * Copyright (C) 2016,深圳市红鸟网络科技股份有限公司 All rights reserved.
@@ -23,10 +25,26 @@ class PasswordInputUIView : BaseItemUIView() {
         return R.layout.item_password_input
     }
 
+    override fun haveSoftInput(): Boolean {
+        return true
+    }
+
+    var keyListener: KeyListener? = null
+
     override fun createItems(items: MutableList<SingleItem>) {
         items.add(object : SingleItem() {
             override fun onBindView(holder: RBaseViewHolder?, posInData: Int, dataBean: Item?) {
-
+                holder?.click(R.id.button) {
+                    val view: PasswordInputEditText = holder!!.v(R.id.password_input_view)
+                    //view.isEnabled = false
+                    if (keyListener == null) {
+                        keyListener = view.keyListener
+                        view.keyListener = null
+                    } else {
+                        view.keyListener = keyListener
+                        keyListener = null
+                    }
+                }
             }
         })
     }
