6cfa6b4873911b482ebcdf2a646adddb26b28f41
angcyo <angcyo@126.com>
2017-12-11 11:40:31 +0800

    fix base

--- uidemo/src/main/java/com/angcyo/uidemo/layout/demo/DYRecordLayoutUIView.kt
+++ uidemo/src/main/java/com/angcyo/uidemo/layout/demo/DYRecordLayoutUIView.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import com.angcyo.uidemo.R
 import com.angcyo.uidemo.layout.base.BaseContentUIView
 import com.angcyo.uiview.container.ContentLayout
+import com.angcyo.uiview.viewgroup.DYRecordView
 
 /**
  * Copyright (C) 2016,深圳市红鸟网络科技股份有限公司 All rights reserved.
@@ -19,6 +20,21 @@ import com.angcyo.uiview.container.ContentLayout
 class DYRecordLayoutUIView : BaseContentUIView() {
     override fun inflateContentLayout(baseContentLayout: ContentLayout?, inflater: LayoutInflater?) {
         inflate(R.layout.view_dy_record_layout)
+    }
+
+    override fun initOnShowContentLayout() {
+        super.initOnShowContentLayout()
+        val recordLayout1 = v<DYRecordView>(R.id.record_view1)
+        val recordLayout2 = v<DYRecordView>(R.id.record_view2)
+        val recordLayout3 = v<DYRecordView>(R.id.record_view3)
+
+        recordLayout3.switchRecordType()
+
+        click(R.id.button) {
+            recordLayout1.switchRecordType()
+            recordLayout2.switchRecordType()
+            recordLayout3.switchRecordType()
+        }
     }
 
 }
