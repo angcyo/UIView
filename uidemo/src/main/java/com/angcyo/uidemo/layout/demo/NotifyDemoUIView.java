536ede22204916f846c41553ccd0d0ee6ceb86a3
angcyo <angcyo@126.com>
2018-03-30 15:44:06 +0800

    fix toast , fix demo

--- uidemo/src/main/java/com/angcyo/uidemo/layout/demo/NotifyDemoUIView.java
+++ uidemo/src/main/java/com/angcyo/uidemo/layout/demo/NotifyDemoUIView.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
+import android.view.Gravity;
 import android.view.View;
 
 import com.angcyo.uidemo.R;
@@ -15,6 +16,9 @@ import com.angcyo.uiview.base.SingleItem;
 import com.angcyo.uiview.recycler.RBaseViewHolder;
 import com.angcyo.uiview.utils.NotifyUtil;
 import com.angcyo.uiview.utils.ProgressNotify;
+import com.angcyo.uiview.utils.T;
+import com.angcyo.uiview.utils.T_;
+import com.angcyo.uiview.utils.Tip;
 
 import java.util.List;
 
@@ -28,33 +32,28 @@ public class NotifyDemoUIView extends BaseItemUIView {
     private Runnable mProgressRunnable;
 
     @Override
-    protected int getItemLayoutId(int viewType) {
-        return R.layout.item_notify;
-    }
-
-    @Override
     protected void createItems(List<SingleItem> items) {
         items.add(new SingleItem() {
             @Override
             public void onBindView(RBaseViewHolder holder, int posInData, Item dataBean) {
-                holder.click(R.id.show_notify, new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        showNotify();
-                    }
-                });
                 holder.click(R.id.notify_normal_singline, new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         new NotifyUtil(mActivity, 10002).notify_normal_singline(getPendingIntent(), R.mipmap.demo_logo,
-                                "ticker", "title", "content", true, true, true);
+                                "ticker", "title", "notify_normal_singline", true, true, true);
                     }
                 });
                 holder.click(R.id.notify_bigPic, new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         new NotifyUtil(mActivity, 10001).notify_bigPic(getPendingIntent(), R.mipmap.demo_logo,
-                                "ticker", "title", "content", R.mipmap.demo_logo, true, true, true);
+                                "ticker", "title", "notify_bigPic", R.mipmap.demo_logo, true, true, true);
+                    }
+                });
+                holder.click(R.id.show_notify, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showNotify();
                     }
                 });
                 holder.click(R.id.show_progress_notify, new View.OnClickListener() {
@@ -81,8 +80,45 @@ public class NotifyDemoUIView extends BaseItemUIView {
                         post(mProgressRunnable);
                     }
                 });
+
+                holder.click(R.id.t1_view, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        T.show(mActivity, "T Toast Test");
+                    }
+                });
+                holder.click(R.id.t2_view, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        T_.info("T_ Toast Test");
+                        postDelayed(1000, new Runnable() {
+                            @Override
+                            public void run() {
+                                T_.error("T_ Toast TestT_ Toast TestT_ Toast TestT_ Toast Test" +
+                                        "T_ Toast TestT_ Toast TestT_ Toast TestT_ Toast Test" +
+                                        "T_ Toast TestT_ Toast TestT_ Toast TestT_ Toast Test" +
+                                        "T_ Toast TestT_ Toast TestT_ Toast TestT_ Toast Test" +
+                                        "T_ Toast TestT_ Toast TestT_ Toast TestT_ Toast TestT_ Toast Test" +
+                                        "T_ Toast TestT_ Toast TestT_ Toast TestT_ Toast TestT_ Toast Test", Gravity.LEFT);
+                            }
+                        });
+                    }
+                });
+                holder.click(R.id.tip_view, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        T_.ok("T_ Toast Test", Gravity.LEFT);
+                        Tip.tip("Tip Toast");
+                    }
+                });
+            }
+
+            @Override
+            public int getItemLayoutId() {
+                return R.layout.item_notify;
             }
         });
+
     }
 
     private PendingIntent getPendingIntent() {
@@ -95,7 +131,7 @@ public class NotifyDemoUIView extends BaseItemUIView {
         NotificationManagerCompat managerCompat = NotificationManagerCompat.from(mActivity);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(mActivity);
 
-        builder.setContentText("content");
+        builder.setContentText("showNotify");
         builder.setContentTitle("title");
         builder.setSmallIcon(R.mipmap.demo_logo);
         builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.demo_logo));//显示在通知内容右边的图片
