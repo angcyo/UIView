6b1b5624d859f702b5d27cd6fab64ebdbe55c1ee
angcyo <angcyo@126.com>
2018-07-20 11:20:45 +0800

    add key board layout

--- uidemo/src/main/java/com/angcyo/uidemo/layout/demo/PasswordInputUIView.kt
+++ uidemo/src/main/java/com/angcyo/uidemo/layout/demo/PasswordInputUIView.kt
@@ -1,11 +1,12 @@
 package com.angcyo.uidemo.layout.demo
 
-import android.text.method.KeyListener
 import com.angcyo.uidemo.R
 import com.angcyo.uidemo.layout.base.BaseItemUIView
 import com.angcyo.uiview.base.Item
 import com.angcyo.uiview.base.SingleItem
 import com.angcyo.uiview.recycler.RBaseViewHolder
+import com.angcyo.uiview.utils.T_
+import com.angcyo.uiview.widget.KeyboardLayout
 import com.angcyo.uiview.widget.PasswordInputEditText
 
 /**
@@ -29,21 +30,30 @@ class PasswordInputUIView : BaseItemUIView() {
         return true
     }
 
-    var keyListener: KeyListener? = null
-
     override fun createItems(items: MutableList<SingleItem>) {
         items.add(object : SingleItem() {
             override fun onBindView(holder: RBaseViewHolder, posInData: Int, dataBean: Item?) {
-                holder?.click(R.id.button) {
-                    val view: PasswordInputEditText = holder!!.v(R.id.password_input_view)
-                    //view.isEnabled = false
-                    if (keyListener == null) {
-                        keyListener = view.keyListener
-                        view.keyListener = null
-                    } else {
-                        view.keyListener = keyListener
-                        keyListener = null
+                val passwordInputEditText: PasswordInputEditText = holder.v(R.id.password_input_view)
+                val keyboardLayout: KeyboardLayout = holder.v(R.id.keyboard_layout)
+
+                keyboardLayout.onKeyboardInputListener = object : KeyboardLayout.OnKeyboardInputListener {
+                    override fun onKeyboardInput(key: String, isDel: Boolean) {
+                        if (isDel) {
+                            passwordInputEditText.delInput()
+                        } else {
+                            passwordInputEditText.insertInput(key)
+                        }
                     }
+                }
+
+                passwordInputEditText.onPasswordInputListener = object : PasswordInputEditText.OnPasswordInputListener {
+                    override fun onPassword(password: String) {
+                        T_.show(password)
+                    }
+                }
+
+                holder.click(R.id.button) {
+                    passwordInputEditText.enablePasswordInput = !passwordInputEditText.enablePasswordInput
                 }
             }
         })
